303a6acafcf9ba2074c55c51b3472bda8c675822
Weston Ruter <westonruter@gmail.com>
2011-08-31 16:25:05 -0700

    Initial commit

---
+++ align.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+"""
+ESV Text/Audio Aligner using CMU Sphinx
+Pulls down the ESV text and audio from the ESV API, then generates
+chapyer-by-chapter {book}.{chapter}.timings.json files in the data/ directory.
+Previously-aligned chapters are skipped unless forced.
+
+Author: Weston Ruter <http://weston.ruter.net/> @westonruter
+Project: https://github.com/westonruter/esv-audio-timings
+Dependencies: Python 2.7, java, ant, sox, svn
+
+The ESV Text and MP3 data downloaded by this script is subject to copyright:
+    The Holy Bible, English Standard Version copyright (c)2001 by Crossway Bibles, a
+    publishing ministry of Good News Publishers. Used by permission. All rights
+    reserved. <http://www.crossway.org/rights-permissions/esv/>
+
+ESV API usage terms available from http://www.esvapi.org/
+    You can access the ESV text using the key "IP" (without the quotes). This
+    key limits you to 5,000 queries per day from a single IP address. You are
+    bound by the below conditions of use, including the non-commercial
+    aspects. <http://www.esvapi.org/#conditions>
+
+USAGE:
+$ python align.py [-f|--force] [osisBook] [chapter, [chapter]...] [osisBook] [chapter, [chapter]...] ...
+
+EXAMPLES:
+
+Align Genesis 1:
+$ python align.py Gen 1
+
+Align John 3, overridding any existing timings:
+$ python align.py -f John 3
+
+Align Genesis 1-3, Psalm 1, and all of Colossians:
+$ python align.py Gen 1 2 3 Ps 1 Col
+
+Dual licensed under the MIT or GPL Version 2 licenses.
+MIT License: http://creativecommons.org/licenses/MIT/
+GPL 2.0 license: http://creativecommons.org/licenses/GPL/2.0/
+"""
+
+import subprocess
+import os
+from os import path
+import urllib
+import codecs
+import sys
+import shutil
+import json
+import re
+from time import time as clock
+import bookinfo
+
+def align(argv):
+    """ Main function for this module (see its docstring for usage) """
+    
+    init_start_time = clock()
+    
+    __dir__ = path.realpath(path.dirname(__file__))
+    sphinx_long_audio_aligner_repo_url = "http://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/branches/long-audio-aligner/Aligner"
+    long_audio_aligner_path = __dir__ + '/long-audio-aligner'
+    data_path = __dir__ + '/data'
+    is_force = ('--force' in sys.argv or '-f' in sys.argv)
+    
+    books = bookinfo.get_book_subset(argv)
+    
+    # svn co http://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/branches/long-audio-aligner/Aligner aligner
+    if not path.exists(long_audio_aligner_path):
+        print "Fetching long-audio-aligner from Sourceforge..."
+        subprocess.call(['svn', 'co', sphinx_long_audio_aligner_repo_url, long_audio_aligner_path])
+    
+    # Remove the initial batchFile since we don't want to process it anyway
+    f = open(long_audio_aligner_path + '/resource/batchFile.txt', 'w')
+    f.write('')
+    f.close()
+    
+    # Build the Java project
+    cwd = os.path.realpath(os.curdir)
+    os.chdir(long_audio_aligner_path)
+    print "Running ant...",
+    retcode = subprocess.call(['ant'])
+    if retcode != 0:
+        print "fail (have you ant?)"
+        sys.exit(1)
+    else:
+        print "done"
+    os.chdir(cwd)
+    
+    # Create the data directory which is where we put all the ESV data: audio, text, HTML, alignments
+    if not path.exists(data_path):
+        print "Making data dir"
+        os.mkdir(data_path)
+    
+    for book in books:
+        print "########################"
+        print "%s (%s)" % (book.name, book.osis)
+        print "########################"
+        
+        book_start_time = clock()
+        
+        for chapter in book.chapters:
+            chapter_start_time = clock()
+            
+            mp3_file = data_path + '/%s.%d.mp3' % (book.osis, chapter)
+            print "%s %d" % (book.osis, chapter)
+            
+            # Fetch MP3
+            if not os.path.exists(mp3_file):
+                mp3_url = 'http://www.esvapi.org/v2/rest/passageQuery?key=IP&output-format=mp3&passage={book}+{chapter}'.format(
+                    book=book.name,
+                    chapter=chapter
+                )
+                print "Downloading MP3...",
+                fi = urllib.urlopen(mp3_url)
+                if not fi.getcode() or fi.getcode() != 200:
+                    print "Failed, getcode =", fi.getcode()
+                    sys.exit(1)
+                else:
+                    print "done"
+                fo = open(mp3_file, 'w')
+                fo.write(fi.read())
+                fi.close()
+                fo.close()
+            else:
+                print "Skipping MP3 (already-fetched)"
+                
+            # Convert to WAV
+            wav_file = mp3_file.replace('.mp3', '.wav')
+            if not os.path.exists(wav_file):
+                print "Generating WAV file from MP3...",
+                retcode = subprocess.call(['sox', mp3_file, wav_file, 'rate', '16k'])
+                if retcode != 0:
+                    print "fail (have you installed SoX?)"
+                    sys.exit(1)
+                else:
+                    print "done"
+            else:
+                print "Skipping WAV (already-generated)"
+            
+            # Fetch text for chapter for Aligner
+            text_file = data_path + '/%s.%d.txt' % (book.osis, chapter)
+            if not path.exists(text_file):
+                print "Fetching text...",
+                params = (
+                    'key=IP',
+                    'output-format=plain-text',
+                    'passage=' + urllib.quote('{book}+{chapter}'.format(book=book.name, chapter=chapter)),
+                    'include-passage-references=false',
+                    'include-first-verse-numbers=false',
+                    'include-verse-numbers=false',
+                    'include-footnotes=false',
+                    'include-short-copyright=false',
+                    'include-passage-horizontal-lines=false',
+                    'include-heading-horizontal-lines=false',
+                    'include-headings=false',
+                    'include-subheadings=false',
+                    'include-selahs=true',
+                    'line-length=0',
+                )
+                text_url = 'http://www.esvapi.org/v2/rest/passageQuery?' + '&'.join(params)
+                fi = urllib.urlopen(text_url)
+                # @todo What is the encoding of the response??
+                if not fi.getcode() or fi.getcode() != 200:
+                    print "Failed, getcode =", fi.getcode()
+                    sys.exit(1)
+                else:
+                    print "done"
+                fo = open(text_file, 'w')
+                chapter_text = fi.read()
+                fo.write(chapter_text)
+                fi.close()
+                fo.close()
+            else:
+                fi = codecs.open(text_file, mode='r', encoding='utf-8')
+                chapter_text = fi.read()
+                fi.close()
+                print "Skipping text (already-fetched)"
+            
+            # Fetch HTML for chapter
+            html_file = data_path + '/%s.%d.html' % (book.osis, chapter)
+            if not path.exists(html_file):
+                print "Fetching HTML...",
+                params = (
+                    'key=IP',
+                    'passage=' + urllib.quote('{book}+{chapter}'.format(book=book.name, chapter=chapter)),
+                    'include-passage-references=false',
+                    'include-first-verse-numbers=false',
+                    'include-verse-numbers=true',
+                    'include-footnotes=true',
+                    'include-surrounding-chapters=false',
+                    'include-audio-link=false',
+                    'include-short-copyright=false',
+                    'include-copyright=true',
+                )
+                html_url = 'http://www.esvapi.org/v2/rest/passageQuery?' + '&'.join(params)
+                fi = urllib.urlopen(html_url)
+                if not fi.getcode() or fi.getcode() != 200:
+                    print "Failed, getcode =", fi.getcode()
+                    sys.exit(1)
+                else:
+                    print "done"
+                fo = open(html_file, 'w')
+                fo.write(fi.read())
+                fi.close()
+                fo.close()
+            else:
+                print "Skipping HTML (already-fetched)"
+            
+            # Create batch file for this chapter
+            f = open(long_audio_aligner_path + '/resource/batchFile.txt', 'w')
+            f.write('../data/{book}.{chapter}.txt ../data/{book}.{chapter}.wav'.format(book=book.osis, chapter=chapter))
+            f.close()
+            
+            # Now run the aligner on the batchFile
+            timings_file = data_path + '/%s.%d.timings.json' % (book.osis, chapter)
+            if not path.exists(timings_file) or is_force:
+                print "Aligning text...",
+                
+                cwd = path.realpath(path.curdir)
+                os.chdir(long_audio_aligner_path)
+                retcode = subprocess.call(['java', '-Xmx3g', '-jar', 'bin/aligner.jar'])
+                if retcode != 0:
+                    print "fail (haz Java?)"
+                    sys.exit(0)
+                else:
+                    print "success"
+                os.chdir(cwd)
+                
+                # Chapter word segments
+                unnormalized_word_chunks = chapter_text.strip().split()
+                
+                # Copy the aligned output into the data directory
+                fi = codecs.open(long_audio_aligner_path + '/timedOutput/1.txt', encoding='utf-8')
+                raw_timings = fi.read().split()
+                fi.close()
+                
+                # Parse the timings out of the raw timings, and then pair up the
+                # normalized word from Sphinx with the actual word from the text
+                timings = []
+                normalize_word_chunk = lambda s: re.sub(r'\W', '', s).lower()
+                stip_punc = lambda s: re.sub(r'^\W+|\W+$', '', s)
+                for raw_timing in raw_timings:
+                    matches = re.match(r'(.+)\((.+),(.+)\)', raw_timing)
+                    word = matches.group(1)
+                    if word == '<unk>':
+                        word = None
+                    else:
+                        skipped_words = 0
+                        while True:
+                            unnormalized_word_chunk = unnormalized_word_chunks.pop(0)
+                            if word == normalize_word_chunk(unnormalized_word_chunk):
+                                word = stip_punc(unnormalized_word_chunk)
+                                break
+                            skipped_words.append(unnormalized_word_chunk)
+                            if len(skipped_words) > 5:
+                                print "Warning! Skipping several words: " + ", ".join(skipped_words)
+                    
+                    timings.append({
+                        "word":  word,
+                        "start": float(matches.group(2)),
+                        "end":   float(matches.group(3)),
+                    })
+                fo = codecs.open(timings_file, mode='w', encoding='utf-8')
+                fo.write(json.dumps(timings, indent=2))
+                fo.close()
+            else:
+                print "Text already aligned"
+            
+            print "Time: %.02fs" % (clock() - chapter_start_time)
+            print "--"
+        
+        print "%s book execution time: %.02fs" % (book.name, clock() - book_start_time)
+        
+    print "Total execution time: %.02fs" % (clock() - init_start_time)
+
+
+# Run tests
+if __name__ == '__main__':
+    align(sys.argv[1:])
---
+++ bookinfo.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+"""
+Basic metadata for books of the Bible
+"""
+
+class BookInfo:
+    def __init__(self, chapters, osis, name):
+        self.chapters = chapters
+        self.osis = osis
+        self.name = name
+
+# @todo This might work better as an OrderedDict, keyed by osisBook
+books = (
+    BookInfo(range(1,50+1),  'Gen',    'Genesis'),
+    BookInfo(range(1,40+1),  'Exod',   'Exodus'),
+    BookInfo(range(1,27+1),  'Lev',    'Leviticus'),
+    BookInfo(range(1,36+1),  'Num',    'Numbers'),
+    BookInfo(range(1,34+1),  'Deut',   'Deuteronomy'),
+    BookInfo(range(1,24+1),  'Josh',   'Joshua'),
+    BookInfo(range(1,21+1),  'Judg',   'Judges'),
+    BookInfo(range(1,4+1),   'Ruth',   'Ruth'),
+    BookInfo(range(1,31+1),  '1Sam',   '1 Samuel'),
+    BookInfo(range(1,24+1),  '2Sam',   '2 Samuel'),
+    BookInfo(range(1,22+1),  '1Kgs',   '1 Kings'),
+    BookInfo(range(1,25+1),  '2Kgs',   '2 Kings'),
+    BookInfo(range(1,29+1),  '1Chr',   '1 Chronicles'),
+    BookInfo(range(1,36+1),  '2Chr',   '2 Chronicles'),
+    BookInfo(range(1,10+1),  'Ezra',   'Ezra'),
+    BookInfo(range(1,13+1),  'Neh',    'Nehemiah'),
+    BookInfo(range(1,10+1),  'Esth',   'Esther'),
+    BookInfo(range(1,42+1),  'Job',    'Job'),
+    BookInfo(range(1,150+1), 'Ps',     'Psalms'),
+    BookInfo(range(1,31+1),  'Prov',   'Proverbs'),
+    BookInfo(range(1,12+1),  'Eccl',   'Ecclesiastes'),
+    BookInfo(range(1,8+1),   'Song',   'Song of Solomon'),
+    BookInfo(range(1,66+1),  'Isa',    'Isaiah'),
+    BookInfo(range(1,52+1),  'Jer',    'Jeremiah'),
+    BookInfo(range(1,5+1),   'Lam',    'Lamentations'),
+    BookInfo(range(1,48+1),  'Ezek',   'Ezekiel'),
+    BookInfo(range(1,12+1),  'Dan',    'Daniel'),
+    BookInfo(range(1,14+1),  'Hos',    'Hosea'),
+    BookInfo(range(1,3+1),   'Joel',   'Joel'),
+    BookInfo(range(1,9+1),   'Amos',   'Amos'),
+    BookInfo(range(1,1+1),   'Obad',   'Obadiah'),
+    BookInfo(range(1,4+1),   'Jonah',  'Jonah'),
+    BookInfo(range(1,7+1),   'Mic',    'Micah'),
+    BookInfo(range(1,3+1),   'Nah',    'Nahum'),
+    BookInfo(range(1,3+1),   'Hab',    'Habakkuk'),
+    BookInfo(range(1,3+1),   'Zeph',   'Zephaniah'),
+    BookInfo(range(1,2+1),   'Hag',    'Haggai'),
+    BookInfo(range(1,14+1),  'Zech',   'Zechariah'),
+    BookInfo(range(1,4+1),   'Mal',    'Malachi'),
+    BookInfo(range(1,28+1),  'Matt',   'Matthew'),
+    BookInfo(range(1,16+1),  'Mark',   'Mark'),
+    BookInfo(range(1,24+1),  'Luke',   'Luke'),
+    BookInfo(range(1,21+1),  'John',   'John'),
+    BookInfo(range(1,28+1),  'Acts',   'Acts'),
+    BookInfo(range(1,16+1),  'Rom',    'Romans'),
+    BookInfo(range(1,16+1),  '1Cor',   '1 Corinthians'),
+    BookInfo(range(1,13+1),  '2Cor',   '2 Corinthians'),
+    BookInfo(range(1,6+1),   'Gal',    'Galatians'),
+    BookInfo(range(1,6+1),   'Eph',    'Ephesians'),
+    BookInfo(range(1,4+1),   'Phil',   'Philippians'),
+    BookInfo(range(1,4+1),   'Col',    'Colossians'),
+    BookInfo(range(1,5+1),   '1Thess', '1 Thessalonians'),
+    BookInfo(range(1,3+1),   '2Thess', '2 Thessalonians'),
+    BookInfo(range(1,6+1),   '1Tim',   '1 Timothy'),
+    BookInfo(range(1,4+1),   '2Tim',   '2 Timothy'),
+    BookInfo(range(1,3+1),   'Titus',  'Titus'),
+    BookInfo(range(1,1+1),   'Phlm',   'Philemon'),
+    BookInfo(range(1,13+1),  'Heb',    'Hebrews'),
+    BookInfo(range(1,5+1),   'Jas',    'James'),
+    BookInfo(range(1,5+1),   '1Pet',   '1 Peter'),
+    BookInfo(range(1,3+1),   '2Pet',   '2 Peter'),
+    BookInfo(range(1,5+1),   '1John',  '1 John'),
+    BookInfo(range(1,1+1),   '2John',  '2 John'),
+    BookInfo(range(1,1+1),   '3John',  '3 John'),
+    BookInfo(range(1,1+1),   'Jude',   'Jude'),
+    BookInfo(range(1,22+1),  'Rev',    'Revelation'),
+)
+
+
+def get_book_subset(args):
+    """
+    Get a subset of BookInfo books; if args is empty, returns all books
+    
+    args -- a list of osisBooks and chapter numbers; the chapter numbers are associated
+    
+    with their immediately-preceding osisBook. If an osisBook appears without any
+    subsequent chapters, then all chapters in the book will be returned.
+    
+    >>> subset_books = get_book_subset('John', '1')
+    >>> len(subset_books)
+    1
+    >>> subset_books[0].osis
+    'John'
+    >>> len(subset_books[0].chapters)
+    1
+    >>> subset_books[0].chapters[0]
+    1
+    >>> subset_books = get_book_subset('Mark', '1', '3', '6', 'Jude')
+    >>> subset_books[0].chapters
+    [1, 3, 6]
+    >>> len(subset_books)
+    2
+    >>> subset_books[1].osis
+    'Jude'
+    >>> subset_books[1].chapters
+    [1]
+    >>> len(get_book_subset())
+    66
+    >>> subset_books = get_book_subset('Mark', '1', 'Mark')
+    Traceback (most recent call last):
+    ...
+    ValueError: You already provided the osisBook 'Mark'
+    >>> subset_books = get_book_subset('1', 'Mark')
+    Traceback (most recent call last):
+    ...
+    ValueError: Expected osisBook. A chapter must be preceded by an osisBook
+    >>> subset_books = get_book_subset('Jude', 2)
+    Traceback (most recent call last):
+    ...
+    ValueError: Chapter '2' does not exist in Jude
+    >>> subset_books = get_book_subset('Matt', 1, 2, 3, 'FOO')
+    Traceback (most recent call last):
+    ...
+    ValueError: Invalid arg 'FOO'. Expected valid osisBook or chapter.
+    >>> subset_books = get_book_subset('Matt', 1, 2, 1)
+    Traceback (most recent call last):
+    ...
+    ValueError: You already provided chapter '1' for Matt
+    
+    """
+    
+    import re
+    from copy import deepcopy
+    from collections import OrderedDict
+    
+    osis_books = [book.osis for book in books]
+    current_osis_book = None
+    subset_book_dict = OrderedDict()
+    for arg in args:
+        # osisBook
+        if arg in osis_books:
+            if arg in subset_book_dict:
+                raise ValueError("You already provided the osisBook '%s'" % arg)
+            current_osis_book = arg
+            subset_book_dict[current_osis_book] = []
+        # chapter
+        elif re.match('^\d+$', str(arg)):
+            if current_osis_book is None:
+                raise ValueError("Expected osisBook. A chapter must be preceded by an osisBook")
+            
+            chapter = int(arg)
+            if chapter not in books[osis_books.index(current_osis_book)].chapters:
+                raise ValueError("Chapter '%d' does not exist in %s" % (chapter, current_osis_book))
+            
+            if chapter in subset_book_dict[current_osis_book]:
+                raise ValueError("You already provided chapter '%d' for %s" % (chapter, current_osis_book))
+            subset_book_dict[current_osis_book].append(chapter)
+        else:
+            raise ValueError("Invalid arg '%s'. Expected valid osisBook or chapter." % arg)
+    
+    if len(subset_book_dict.keys()) == 0:
+        return books
+    else:
+        subset_books = []
+        for osis_book, chapters in subset_book_dict.iteritems():
+            if len(chapters) == 0:
+                subset_books.append(books[osis_books.index(osis_book)])
+            else:
+                book_info = deepcopy(books[osis_books.index(osis_book)])
+                book_info.chapters = chapters
+                subset_books.append(book_info)
+        return subset_books
+
+
+# Run tests
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
